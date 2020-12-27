365bb50f2b1bb16ab2a65e7b196a1075e87a0fc5
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2020-12-27 18:36:16 -0300

    index
    pagina inicial

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>High School</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
 </body>
+<section id="bg">
+  <div class="container">
+    <div class="title">
+      <h1>High School</h1>
+      <h3>EDUCAR COM AMOR</h3>
+    </div>
+    <div class="buttons">
+      <a href="" id="btn-login">ENTRAR</a>
+      <a href="" id="btn-recovery">RECUPERAR SENHA</a>
+    </div>
+  </div>
+</section>
 
 </html>
